f7f00aebce475263f7710512ea52233dc94ac959
Nicholas <nabraun03@gmail.com>
2024-04-21 23:04:36 -0400

    Remove old files

--- main.py
+++
@@ -1,9 +0,0 @@
-
-scripts = ['apirequests.py', 'preprocessing.py', 'currentprofiles.py', 'predictions.py']
-
-
-for script in scripts:
-    print(script)
-    with open(script, "r") as file:
-        exec(file.read())
-
